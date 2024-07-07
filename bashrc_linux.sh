f1535cada5612906a2f30510e8666fd62dae7bd9
Tammer Ibrahim <tammer.ibrahim@pennmedicine.upenn.edu>
2024-07-07 00:14:15 -0400

    update

--- bashrc_linux.sh
+++ bashrc_linux.sh
@@ -1,5 +1,5 @@
 # use .local/bin for executables
-export PATH="/usr/bin:/usr/local/bin:$HOME/.local/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 
 # Start keychain and add the specified keys
 eval "$(keychain --eval --agents ssh id_rsa)"
