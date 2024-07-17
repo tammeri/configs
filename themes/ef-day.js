93886ae30bcf1304b3b50fefeafc3468f539eb69
Tammer Ibrahim <tammer.ibrahim@pennmedicine.upenn.edu>
2024-07-17 00:05:38 -0400

    updates, switch to ef-winter

---
+++ themes/ef-day.js
@@ -0,0 +1,26 @@
+black       = '#000000';
+red         = '#ba2d2f'; // red
+green       = '#007a0a'; // green
+yellow      = '#a45a22'; // yellow
+blue        = '#375cc6'; // blue
+magenta     = '#ca3e54'; // pink
+cyan        = '#3f60af'; // cyan
+white       = '#a6a6a6'; // light gray
+lightBlack  = '#5c5c5c';
+lightRed         = '#ce3f00'; // red
+lightGreen       = '#5a7400'; // green
+lightYellow      = '#aa4f30'; // yellow
+lightBlue        = '#5f5fdf'; // blue
+lightMagenta     = '#8448aa'; // pink
+lightCyan        = '#0f7b8f'; // cyan
+lightWhite       = '#e5e5e5'; // light gray
+
+t.prefs_.set('color-palette-overrides',
+             [ black , red     , green  , yellow ,
+               blue     , magenta , cyan   , white ,
+	       lightBlack   , lightRed  , lightGreen , lightYellow,
+               lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(207, 31, 0, 0.5)');
+t.prefs_.set('foreground-color', '#584141');
+t.prefs_.set('background-color', '#fff5ea');
---
+++ themes/ef-spring.js
@@ -0,0 +1,26 @@
+black       = '#00000';
+red         = '#c42d2f'; // red
+green       = '#1a870f'; // green
+yellow      = '#b6540f'; // yellow
+blue        = '#5f5fdf'; // blue
+magenta     = '#d5206f'; // pink
+cyan        = '#1f6fbf'; // cyan
+white       = '#a6a6a6'; // light gray
+lightBlack  = '#595959';
+lightRed         = '#cf2f4f'; // red
+lightGreen       = '#007f68'; // green
+lightYellow      = '#a45f22'; // yellow
+lightBlue        = '#265fbf'; // blue
+lightMagenta     = '#d5206f'; // pink
+lightCyan        = '#0f7b8f'; // cyan
+lightWhite       = '#e5e5e5'; // light gray
+
+t.prefs_.set('color-palette-overrides',
+             [ black , red     , green  , yellow ,
+               blue     , magenta , cyan   , white ,
+	       lightBlack   , lightRed  , lightGreen , lightYellow,
+               lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(191, 0, 95, 0.5)');
+t.prefs_.set('foreground-color', '#34494a');
+t.prefs_.set('background-color', '#f6fff9');
